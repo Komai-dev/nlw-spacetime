e992870e38bbd403693929df2177ca0083e07cf1
Maiko Costa <122563962+Komai-dev@users.noreply.github.com>
2023-05-16 13:47:17 -0300

    asdwadas

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -4,12 +4,12 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/hello', () => {
+app.post('/hello', () => {
   return 'Hello World'
 })
 
 app.listen({
   port: 3333,
 }).then(() => {
-  console.log('HTTP server running on http://localhost:3333')
+  console.log('🚀 HTTP server running on http://localhost:3333')
 })

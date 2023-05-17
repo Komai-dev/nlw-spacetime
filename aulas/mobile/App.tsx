92e32151726fff0e0b8698c33952c2f2d39dee2e
Maiko Costa <122563962+Komai-dev@users.noreply.github.com>
2023-05-17 12:17:20 -0300

    aaaaa

--- aulas/mobile/App.tsx
+++ aulas/mobile/App.tsx
@@ -3,23 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
+    <View>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color:'#fff',
-    fontWeight: '700',
-    fontSize: 48,
-  },
-});

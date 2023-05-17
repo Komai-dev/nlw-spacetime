eedd5ccf6270df8974ceeeb964e1545aff5c4a24
Maiko Costa <122563962+Komai-dev@users.noreply.github.com>
2023-05-16 22:09:19 -0300

    x

--- aulas/web/src/app/page.tsx
+++ aulas/web/src/app/page.tsx
@@ -2,7 +2,7 @@
 // JSX = Java Script + XML (sintaxe do html) //
 export default function Home() {
   return (
-    <div className="text-2xl font-bold uppercase w-20 h-20 bg-red-300 rounded-3xl m-10">
+    <div className="h-screen bg-zinc-900">
       <p className="text-center align-middle">oi</p>
     </div>
   )

4def2a9f1ad7e1c61351ab82b72642e7fa1ee4cb
Maiko Costa <122563962+Komai-dev@users.noreply.github.com>
2023-05-16 21:01:35 -0300

    teste button

--- aulas/web/src/app/page.tsx
+++ aulas/web/src/app/page.tsx
@@ -1,10 +1,13 @@
 // TypeScript + JSX = TSX //
 // JSX = Java Script + XML (sintaxe do html) //
 
+import { Button } from "@/components/button";
+
 export default function Home() {
   return (
     <h1>
       Hello World
+      <Button />
     </h1>
   )
 }

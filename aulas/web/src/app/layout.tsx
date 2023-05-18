a26dd4b6833789b3f76ef15b83d1fc57544562be
Maiko Costa <122563962+Komai-dev@users.noreply.github.com>
2023-05-18 19:33:46 -0300

    bb

--- aulas/web/src/app/layout.tsx
+++ aulas/web/src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={roboto.variable}>{children}</body>
+      <body className={`${roboto.variable} font-sans`}>{children}</body>
     </html>
   )
 }

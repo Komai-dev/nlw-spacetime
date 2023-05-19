50a8e7d69d42d7589ff5016b2da091e069b35309
Maiko Costa <122563962+Komai-dev@users.noreply.github.com>
2023-05-19 11:35:55 -0300

    fazendo cagada (observação)

--- aulas/server/src/routes/memories.ts
+++ aulas/server/src/routes/memories.ts
@@ -2,9 +2,28 @@ import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 
 export async function memoriesRoutes(app: FastifyInstance) {
-  app.get('/users', async () => {
-    const users = await prisma.user.findMany()
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      }
+    })
+    return memories
+  })
 
-    return users
+  app.get('/memories/:id', async () => {
+
+  })
+
+  app.post('/memories', async () => {
+
+  })
+
+  app.put('/memories/:id', async () => {
+
+  })
+
+  app.delete('/memories/:id', async () => {
+
   })
 }

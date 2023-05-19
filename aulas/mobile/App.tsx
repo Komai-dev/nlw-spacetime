bbe10537cc4f0178cc55fd7d133be756ccd352f2
Maiko Costa <122563962+Komai-dev@users.noreply.github.com>
2023-05-19 00:37:10 -0300

    a

--- aulas/mobile/App.tsx
+++ aulas/mobile/App.tsx
@@ -9,6 +9,10 @@ import {
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import { styled } from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -27,6 +31,7 @@ export default function App() {
       className="relative flex-1 items-center bg-gray-900"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
+      <StyledStripes className="absolute left-2" />
       <StatusBar style="light" translucent />
     </ImageBackground>
   )
